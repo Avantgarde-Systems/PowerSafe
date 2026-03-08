c9c679cbd6aa6742e15e730e0ebc1dfbf924fd9d
Yurii <yuriydorosh2005@gmail.com>
2026-03-08 11:51:26 +0200

    FIX: celery and flower by fixing python path and celery_app.py file

--- backend/docker_compose/worker_1/entrypoint.sh
+++ backend/docker_compose/worker_1/entrypoint.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-celery -A src.celery_app.app worker -l info --hostname=worker1@%h
+# celery -A src.celery_app.app worker -l info --hostname=worker1@%h
+celery -A src.celery_app:app worker -l info --hostname=worker1@%h
